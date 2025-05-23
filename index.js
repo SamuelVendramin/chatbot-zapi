4b4416c8b482d257e805c16b69a8e09391a46bf0
SamuelVendramin <cazalimpaecia@gmail.com>
2025-05-23 01:35:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
-
-const express = require("express");
 const axios = require("axios");
 const app = express();
 
